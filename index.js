9404ab76cf7ae4d74d9aae164ca46490a916365f
Bonventure Maeta Oanda <64786006+ADMBonny@users.noreply.github.com.>
2023-09-24 22:20:21 +0300

    add index.js, add  index.html, add index.css

---
+++ index.js
@@ -0,0 +1,8 @@
+<script>
+    const button = document.createElement("button");
+    button.textContent = "Click Me!"
+    document.body.appendChild(button);
+    button.addEventListener("click", () = { 
+        alert("Hello, I'm Bonventure Maeta!");
+        });
+</script>
