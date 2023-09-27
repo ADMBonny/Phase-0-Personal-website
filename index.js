5f4b05135167bc7a8c0ef342b8166a2cbd9bc6c9
Bonventure Maeta Oanda <64786006+ADMBonny@users.noreply.github.com.>
2023-09-27 13:44:08 +0300

    add

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const contactForm = document.getElementById("contact-form");
+    const emailInput = document.getElementById("email");
+    const messageInput = document.getElementById("message");
+    const messageButton = document.getElementById("message-button");
 
-           document.addEventListener("DOMContentLoaded", function () {
-            const button = document.createElement("button");
-            button.textContent = "Click Me!";
-            document.body.appendChild(button);
+    contactForm.addEventListener("submit", function (e) {
+        e.preventDefault();
 
-            button.addEventListener("click", function () {
-                alert("Hello, I'm Bonenture Maeta!");
-            });
-        });      
-
-
+        // Here, you can add code to handle the form submission, like sending an email or storing the message in a database.
+        // For now, let's just alert the user.
+        alert("Message sent successfully!");
+        emailInput.value = "";
+        messageInput.value = "";
+    });
+});
 
 
     
